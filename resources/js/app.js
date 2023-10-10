2b37af04076181b7fae1a68a34ff77445057233e
Marcel Stadelmann <m@marceli.to>
2023-10-10 11:12:19 +0200

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,4 @@ import './modules/fancybox.js';
 import './modules/maps.js';
 import './modules/cta.js';
 import './modules/filter.js';
+import './modules/iso.js';
---
+++ resources/views/components/apartments/iso/wrapper.blade.php
@@ -0,0 +1,10 @@
+@props(['number'])
+@if ($number == 1)
+  <x-apartments.iso.buildings.1 />
+@endif
+@if ($number == 2)
+  <x-apartments.iso.buildings.2 />
+@endif
+@if ($number == 3)
+  <x-apartments.iso.buildings.3 />
+@endif
