0da49e5118120451d5e0a2c42914a0932daa69de
Marcel Stadelmann <m@marceli.to>
2023-10-10 13:04:02 +0200

    wip

--- resources/js/modules/iso.js
+++ resources/js/modules/iso.js
@@ -35,26 +35,6 @@ const Iso = (function() {
 
   const listOver = function(apartment) {
     const number = apartment.dataset.number;
-
-    // add class is-opaque to all iso items except the one that matches the apartment number
-    // document.querySelectorAll(selectors.isoItem).forEach(function(isoItem) {
-    //   if (isoItem.dataset.isoItem !== number) {
-    //     isoItem.classList.add('is-opaque');
-    //   }
-    // });
-
-    // // add class is-opaque to all iso structure items
-    // document.querySelectorAll(selectors.isoStructure).forEach(function(isoStructure) {
-    //   isoStructure.classList.add('is-opaque');
-    // });
-
-    // const isoItem = document.querySelector('[data-iso-item="' + number + '"]');
-    // if (isoItem) {
-    //   isoItem.classList.add('is-highlighted');
-    // }
-
-
-
     const isoItem = document.querySelector('[data-iso-item="' + number + '"]');
 
     if (isoItem) {
@@ -65,26 +45,15 @@ const Iso = (function() {
     const parent = isoItem.parentElement;
 
     // find all siblings of the parent <g> element that are after it
-    const siblings = [];
-    let nextSibling = parent.nextElementSibling;
-    while (nextSibling) {
-      siblings.push(nextSibling);
-      nextSibling = nextSibling.nextElementSibling;
-    }
+    const nextSiblings = getNextSiblings(parent);
 
-    // add styles to translate the parent <g> element and all its siblings
-    parent.classList.add('is-up')
-    siblings.forEach(function(sibling) {
+    // add styles to translate the parent <g> elements siblings
+    nextSiblings.forEach(function(sibling) {
       sibling.classList.add('is-up')
     });
 
     // find all siblings of the parent <g> element that are before it
-    const previousSiblings = [];
-    let previousSibling = parent.previousElementSibling;
-    while (previousSibling) {
-      previousSiblings.push(previousSibling);
-      previousSibling = previousSibling.previousElementSibling;
-    }
+    const previousSiblings = getPreviousSiblings(parent);
 
     // add styles to translate the parent <g> element and all its siblings
     parent.classList.add('is-down')
@@ -96,18 +65,8 @@ const Iso = (function() {
 
   const listOut = function(apartment) {
     const number = apartment.dataset.number;
-
-    // // remove class is-opaque from all iso items
-    // document.querySelectorAll(selectors.isoItem).forEach(function(isoItem) {
-    //   isoItem.classList.remove('is-opaque');
-    // });
-
-    // // remove class is-opaque to all iso structure items
-    // document.querySelectorAll(selectors.isoStructure).forEach(function(isoStructure) {
-    //   isoStructure.classList.remove('is-opaque');
-    // });
-
     const isoItem = document.querySelector('[data-iso-item="' + number + '"]');
+
     if (isoItem) {
       isoItem.classList.remove('is-highlighted');
     }
@@ -116,6 +75,38 @@ const Iso = (function() {
     const parent = isoItem.parentElement;
 
     // find all siblings (before and after) of the parent <g> element
+    const siblings = getAllSiblings(parent);
+
+    // remove all instances of the is-up and is-down classes
+    parent.classList.remove('is-up');
+    parent.classList.remove('is-down');
+    siblings.forEach(function(sibling) {
+      sibling.classList.remove('is-up');
+      sibling.classList.remove('is-down');
+    });
+  };
+
+  const getNextSiblings = (parent) => {
+    const siblings = [];
+    let nextSibling = parent.nextElementSibling;
+    while (nextSibling) {
+      siblings.push(nextSibling);
+      nextSibling = nextSibling.nextElementSibling;
+    }
+    return siblings;
+  };
+
+  const getPreviousSiblings = (parent) => {
+    const previousSiblings = [];
+    let previousSibling = parent.previousElementSibling;
+    while (previousSibling) {
+      previousSiblings.push(previousSibling);
+      previousSibling = previousSibling.previousElementSibling;
+    }
+    return previousSiblings;
+  };
+
+  const getAllSiblings = (parent) => {
     const siblings = [];
     let nextSibling = parent.nextElementSibling;
     while (nextSibling) {
@@ -127,48 +118,8 @@ const Iso = (function() {
       siblings.push(previousSibling);
       previousSibling = previousSibling.previousElementSibling;
     }
-
-    // remove all instances of the is-up and is-down classes
-    parent.classList.remove('is-up');
-    parent.classList.remove('is-down');
-    siblings.forEach(function(sibling) {
-      sibling.classList.remove('is-up');
-      sibling.classList.remove('is-down');
-    });
-
-    // // find all siblings of the parent <g> element that are after it
-    // const siblings = [];
-    // let nextSibling = parent.nextElementSibling;
-    // while (nextSibling) {
-    //   siblings.push(nextSibling);
-    //   nextSibling = nextSibling.nextElementSibling;
-    // }
-
-    // // add styles to translate the parent <g> element and all its siblings
-    // //parent.style.transform = 'translateY(0)';
-    // parent.classList.remove('is-up');
-    // siblings.forEach(function(sibling) {
-    //   //sibling.style.transform = 'translateY(0)';
-    //   sibling.classList.remove('is-up')
-    // });
-
-    // // find all siblings of the parent <g> element that are before it
-    // const previousSiblings = [];
-    // let previousSibling = parent.previousElementSibling;
-    // while (previousSibling) {
-    //   previousSiblings.push(previousSibling);
-    //   previousSibling = previousSibling.previousElementSibling;
-    // }
-
-    // // add styles to translate the parent <g> element and all its siblings
-    // //parent.style.transform = 'translateY(0)';
-    // parent.classList.remove('is-down');
-    // previousSiblings.forEach(function(sibling) {
-    //   //sibling.style.transform = 'translateY(0)';
-    //   sibling.classList.remove('is-down')
-    // });
+    return siblings;
   };
-
 
   // RETURN PUBLIC METHODS
   return {
