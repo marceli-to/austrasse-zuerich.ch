93cd8100f13535f7d7234aca5066ab3396186f1e
Marcel Stadelmann <m@marceli.to>
2023-10-12 22:49:38 +0200

    wip

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -40,8 +40,8 @@
   </x-layout.section>
 </footer>
 @livewireScripts
-<script src='https://api.mapbox.com/mapbox-gl-js/v1.3.1/mapbox-gl.js'></script>
-<link href='https://api.mapbox.com/mapbox-gl-js/v1.3.1/mapbox-gl.css' rel='stylesheet' />
+<script src='https://api.mapbox.com/mapbox-gl-js/v1.3.1/mapbox-gl.js' defer></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v1.3.1/mapbox-gl.css' defer rel='stylesheet' />
 @vite('resources/js/app.js')
 </body>
 <!-- made with ❤ by stoz.ch & marceli.to -->
