1bd767112cd8e9bf195bf78ce1914da2fe912b0e
Marcel Stadelmann <m@marceli.to>
2023-10-22 22:35:57 +0200

    wip

--- resources/js/modules/maps.js
+++ resources/js/modules/maps.js
@@ -52,32 +52,31 @@ const loadMapScript = () => {
   }
 }
 
-loadMapScript();
+//loadMapScript();
 
-// const isInViewport = (selector) => {
-//   const mapEl = document.getElementById(selector);
-//   if (mapEl === null) {
-//     return;
-//   }
-//   const rect = mapEl.getBoundingClientRect();
-//   const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-//   const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
-//   const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
-//   const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
-//   return (vertInView && horInView);
-// };
+const isInViewport = (selector) => {
+  const mapEl = document.getElementById(selector);
+  if (mapEl === null) {
+    return;
+  }
+  const rect = mapEl.getBoundingClientRect();
+  const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+  const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+  const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+  const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+  return (vertInView && horInView);
+};
 
-// if (isInViewport('map')) {
-//   loadMapScript();
-// }
+if (isInViewport('map')) {
+  loadMapScript();
+}
 
-
-// const scrollEventHandler = () => {
-//   if (isInViewport('map')) {
-//     loadMapScript();
-//     window.removeEventListener('scroll', scrollEventHandler);
-//   }
-// }
-// window.addEventListener('scroll', scrollEventHandler);
+const scrollEventHandler = () => {
+  if (isInViewport('map')) {
+    loadMapScript();
+    window.removeEventListener('scroll', scrollEventHandler);
+  }
+}
+window.addEventListener('scroll', scrollEventHandler);
 
 
